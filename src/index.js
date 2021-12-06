fec707f7d2f4b624884976a83402efaff02a3b56
lpoole <keithpoole123@gmail.com>
2021-12-06 00:02:56 -0500

    added sickass new nintendo start screen

--- src/index.js
+++ src/index.js
@@ -2,21 +2,28 @@ const Game = require("./game");
 const Board = require("./board");
 const Player = require("./player");
 const Enemy = require("./enemy");
-// const GameView = require("./game_view");
+
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("#welcome-page").classList.remove("hidden");
+  let btn = document.createElement("button");
+  btn.setAttribute("id", "btn");
+  // btn.innerHTML = "Start";
+  btn.addEventListener("click", function () {
+    game.animateGame();
+    document.querySelector("#welcome-page").classList.add("hidden");
+    document.querySelector("#game-page").classList.remove("hidden");
+    document.querySelector("#game-system").classList.add("hidden");
+  });
+  document.body.appendChild(btn);
+  // document.querySelector("#welcome-page").classList.remove("hidden");
   const canvasEl = document.getElementsByTagName("canvas")[0];
-  
+
   canvasEl.width = Game.DIM_X;
   canvasEl.height = Game.DIM_Y;
-  
+
   const ctx = canvasEl.getContext("2d");
   const game = new Game(ctx);
-  const pImg = document.getElementById('source');
+  const pImg = document.getElementById("source");
 
-     
-
-  
   window.ctx = ctx;
 
   window.Board = Board;
@@ -26,43 +33,39 @@ document.addEventListener("DOMContentLoaded", function () {
   window.Game = Game;
   window.image = pImg;
   console.log("Webpack is working!");
-  // new GameView(game, ctx).start();
+
   document.addEventListener("keydown", (e) => {
     e.preventDefault();
-    // if (start) {
-      const canvasEl = document.getElementsByTagName("canvas")[0];
+
+    const canvasEl = document.getElementsByTagName("canvas")[0];
     const ctx = canvasEl.getContext("2d");
-    // debugger;
-      switch (e.key) {
-        case "ArrowUp":
-          game.player.playerMove("up");
-          game.drawGame(ctx);
-          break;
-        case "ArrowDown":
-          game.player.playerMove("down");
-          game.drawGame(ctx);
-          break;
-        case "ArrowLeft":
-          game.player.playerMove("left");
-          game.drawGame(ctx);
-          break;
-        case "ArrowRight":
-          game.player.playerMove("right");
-          game.drawGame(ctx);
-          break;
-        case " ":
-          // game.player.playerMove(" ");
-          game.fireWeapon();
-          game.drawGame(ctx);
-          break;
-        case "Enter":
-          game.animateGame();
-          document.querySelector("#welcome-page").classList.add("hidden");
-          document.querySelector("#game-page").classList.remove("hidden");
-          break;
-      // }
+
+    switch (e.key) {
+      case "ArrowUp":
+        game.player.playerMove("up");
+        game.drawGame(ctx);
+        break;
+      case "ArrowDown":
+        game.player.playerMove("down");
+        game.drawGame(ctx);
+        break;
+      case "ArrowLeft":
+        game.player.playerMove("left");
+        game.drawGame(ctx);
+        break;
+      case "ArrowRight":
+        game.player.playerMove("right");
+        game.drawGame(ctx);
+        break;
+      case " ":
+        game.fireWeapon();
+        game.drawGame(ctx);
+        break;
+      // case "Enter":
+      //   game.animateGame();
+      //   document.querySelector("#welcome-page").classList.add("hidden");
+      //   document.querySelector("#game-page").classList.remove("hidden");
+      //   break;
     }
   });
 });
-
-
